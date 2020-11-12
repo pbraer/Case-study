0724dce6b43d3d2f06ea1088d6b509e059624839
novoselovvasily <novoselov020@mail.ru>
2020-11-12 21:38:44 +0700

    changed

--- main.py
+++ main.py
@@ -15,12 +15,22 @@ name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OC
 question='Доход:'
 annual_income = 0
 for month in range(12):
-    print('{} {}:'.format(question,name_month[month], end ='' )
+    print('{} {}:'.format(question,name_month[month], end ='' ))
     income = float(input())
     annual_income += income
 print(annual_income)
-n1=s1*(d-d1)
-n2=s1*(d2-d1)+s2*(d-d2)
-n3=s1*(d2-d1)+s2*(d3-d2)+s3*(d-d3)
-n4=s1*(d2-d1)+s2*(d3-d2)+s3*(d4-d3)+s4(d-d4)
-n5=s1*(d2-d1)+s2*(d3-d2)+s3*(d4-d3)+s4(d5-d4)+s5(d-d5)
+n1 = s1 * (d - d1)
+n2 = s1 * (d2 - d1) + s2 * (d - d2)
+n3 = s1 * (d2 - d1) + s2 * (d3 - d2) + s3 * (d - d3)
+n4 = s1 * (d2 - d1) + s2 * (d3 - d2) + s3 * (d4 - d3) + s4 * (d - d4)
+n5 = s1 * (d2 - d1) + s2 * (d3 - d2) + s3 * (d4 - d3) + s4 * (d5 - d4) + s5 * (d - d5)
+n6 = s1 * (d2 - d1) + s2 * (d3 - d2) + s3 * (d4 - d3) + s4 * (d5 - d4) + s5 * (d6 - d5) + s6 * (d - d6)
+n7 = s1 * (d2 - d1) + s2 * (d3 - d2) + s3 * (d4 - d3) + s4 * (d5 - d4) + s5 * (d6 - d5) + s6 * (d7 - d6) + s7 * (d - d7)
+subject=input('Семейный статус? ')
+#для одного субъекта
+if subject.lower()=='один субъект' :
+    //////////
+    elif subject.lower()=='семейная пара' :
+        ////////////
+    elif subject.lower()=='родитель-одиночка' :
+        /////////
