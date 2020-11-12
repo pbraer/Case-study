4bf99338aa92464657d6b60c9ebc8e4533e58631
novoselovvasily <novoselov020@mail.ru>
2020-11-12 19:53:43 +0700

    the list

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+# Case-study #2
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Vasily N. (%)
+
+# string constants
 name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+
 annual_income = 0
 for month in range(12):
-    print('{} {}:'.format(QUESTION,name_month[month], end ='')
+    print('{} {}:'.format(QUESTION, name_month[month], end ='')
     income = float(input())
     annual_income += income
 print(annual_income)
