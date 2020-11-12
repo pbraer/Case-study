e266620681e127682d2dd198e9c3478c7204a438
novoselovvasily <novoselov020@mail.ru>
2020-11-12 20:53:08 +0700

    p1

--- main.py
+++ main.py
@@ -2,16 +2,6 @@
 # Developers:   Braer P. (%),
 #               Kokorina D. (%),
 #               Vasily N. (%)
-
-# string constants
-name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
-
-annual_income = 0
-for month in range(12):
-    print('{} {}:'.format(QUESTION, name_month[month], end ='')
-    income = float(input())
-    annual_income += income
-print(annual_income)
 s1=0.1
 s2=0.15
 s3=0.25
@@ -20,3 +10,12 @@ s5=0.33
 s6=0.35
 s7=0.396
 nalv=input("Налоговый вычет: ")
+# string constants
+name_month = ['JAN', 'FAB', 'MAR', 'APR', 'MAY', 'JUN', 'JUL', 'AUG', 'SEP', 'OCT', 'NOV', 'DEC']
+question='Доход:'
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(question,name_month[month], end ='' )
+    income: object = float(input())
+    annual_income += income
+print(annual_income)
