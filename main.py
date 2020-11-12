d41a240917bd1d6e719a3bd0225dd4af2384a7ac
novoselovvasily <novoselov020@mail.ru>
2020-11-12 20:08:36 +0700

    ver

--- main.py
+++ main.py
@@ -12,3 +12,11 @@ for month in range(12):
     income = float(input())
     annual_income += income
 print(annual_income)
+s1=0.1
+s2=0.15
+s3=0.25
+s4=0.28
+s5=0.33
+s6=0.35
+s7=0.396
+nalv=input("Налоговый вычет: ")
