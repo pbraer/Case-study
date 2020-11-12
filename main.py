7ecb6c4189d3ffe8796cbf8dbf5b1e6173f1318d
pbraer <73088589+pbraer@users.noreply.github.com>
2020-11-12 19:51:41 +0700

    the list of developers

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print ('Hola')
+# Case-study #2
+# Developers:   Braer P. (%),
+#               Kokorina D. (%),
+#               Vasily S. (%)
+
+# string constants
+name_month = [JAN, FAB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC]
+
+annual_income = 0
+for month in range(12):
+    print('{} {}:'.format(QUESTION, name_month[month], end ='')
+    income = float(input())
+    annual_income += income
+print(annual_income)
